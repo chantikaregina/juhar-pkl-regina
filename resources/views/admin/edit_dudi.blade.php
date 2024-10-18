31462635e5d673f5629d490e887babfa3755b76f
chantikaregina <reginachantika50@gmail.com>
2024-10-18 16:19:12 +0700

    membuat hapus, edit guru dan crud dudi

---
+++ resources/views/admin/edit_dudi.blade.php
@@ -0,0 +1,39 @@
+@extends('admin.layouts.app')
+
+@section('title', 'Tambah dudi')
+
+@section('content')
+<div class="row g-4">
+    <div class="col-sm-12">
+        <div class="bg-light rounded h-100 p-4">
+            <h6 class="mb-4">Tambah dudi</h6>
+            <form action="{{ route('admin.dudi.store') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    <label for="nama_dudi" class="form-label">nama</label>
+                    <input type="text" class="form-control" id="nama_dudi" name="nama_dudi" value="{{ old('email', $dudi->nama_dudi) }}">
+                    <div class="text-danger">
+                        @error('nama_dudi')
+                        {{$message}}
+
+                        @enderror
+                    </div>
+
+
+                </div>
+                <div class="mb-3">
+                    <label for="alamat_dudi" class="form-label">alamat</label>
+                    <input type="text" class="form-control" id="alamat_dudi" name="alamat_dudi" value="{{ old('email', $dudi->alamat_dudi) }}">
+                    <div class="text-danger">
+                        @error('alamat_dudi')
+                        {{ $message }}
+
+                        @enderror
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">simpan</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
